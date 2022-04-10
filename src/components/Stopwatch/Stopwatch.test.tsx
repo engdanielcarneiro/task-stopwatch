b57f8253ca2feaf5bc03565839fb44680d1d46d6
Daniel Carneiro <daniel.alexandre97@hotmail.com>
2022-04-10 18:30:40 -0300

    feat: adding beforeEach and afterEach to preserve tests quality

--- src/components/Stopwatch/Stopwatch.test.tsx
+++ src/components/Stopwatch/Stopwatch.test.tsx
@@ -1,15 +1,24 @@
-import { fireEvent, render } from "@testing-library/react";
+import { cleanup, fireEvent, render } from "@testing-library/react";
 import Stopwatch from ".";
+
+let startButton: HTMLElement;
+const endTaskFunction = jest.fn();
+
+afterEach(() => {
+  cleanup();
+});
+
+beforeEach(() => {
+  const { getByText } = render(
+    <Stopwatch dataTestId="stopwatch" endTask={endTaskFunction} />
+  );
+  startButton = getByText("Start!");
+});
 
 describe("Stopwatch component", () => {
   test("Start button triggers countdown function", () => {
     const spy = jest.spyOn(console, "log");
 
-    const endTaskFunction = jest.fn();
-    const { getByText } = render(
-      <Stopwatch dataTestId="stopwatch" endTask={endTaskFunction} />
-    );
-    const startButton = getByText("Start!");
     fireEvent.click(startButton);
 
     expect(spy).toHaveBeenCalled();
--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,16 +1,28 @@
-import { render } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import Home from ".";
 
 const renderHomePage = () => render(<Home />);
 
-describe("When Home page is rendered", () => {
-  let { getByText, getByTestId } = renderHomePage();
-  const form = getByTestId("form");
-  const taskList = getByTestId("task-list");
-  const stopwatch = getByTestId("stopwatch");
+var title: HTMLElement,
+  form: HTMLElement,
+  taskList: HTMLElement,
+  stopwatch: HTMLElement;
 
+afterEach(() => {
+  cleanup();
+});
+
+beforeEach(() => {
+  let { getByText, getByTestId } = renderHomePage();
+  title = getByText("Alura Studying Guide");
+  form = getByTestId("form");
+  taskList = getByTestId("task-list");
+  stopwatch = getByTestId("stopwatch");
+});
+
+describe("When Home page is rendered", () => {
   test("The app name is shown", () => {
-    expect(getByText("Alura Studying Guide")).toBeInTheDocument();
+    expect(title).toBeInTheDocument();
   });
 
   test("Form, List and Stopwatch were rendered correctly", () => {
